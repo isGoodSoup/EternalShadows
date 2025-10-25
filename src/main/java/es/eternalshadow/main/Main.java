3beee25ac114bdcede46a61d91762b89f57a49c5
isGoodSoup <sargent.pointed453@passinbox.com>
2025-10-25 11:44:36 +0200

    feat: parte gráfica eliminada

--- src/main/java/es/eternalshadow/main/Main.java
+++ src/main/java/es/eternalshadow/main/Main.java
@@ -1,28 +1,13 @@
 package es.eternalshadow.main;
 
-import javax.swing.JFrame;
-
 public class Main {
-	private String title = "EternalShadows";
-	private JFrame frame = new JFrame();
-	private Panel panel = new Panel();
-	
+
 	public static void main(String[] args) {
 		Main main = new Main();
 		main.run();
 	}
 	
 	public void run() {
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setResizable(true);
-		frame.setTitle(this.title);
-		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		frame.setUndecorated(true);
-		frame.add(panel);
-		frame.pack();
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
-		panel.requestFocusInWindow();
-		new Thread(panel).start();
+		
 	}
 }
